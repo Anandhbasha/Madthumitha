769cfdbdd562af9c30f861a881b26cd1f4bd49ac
Anandhbasha <anadhakm@gmail.com>
2025-06-24 13:15:56 +0530

    print

---
+++ index.py
@@ -0,0 +1,8 @@
+# print("Hello python is working")
+a=10
+print("Hello Python is working", end=" ")
+# /n - new like
+print(a)
+print("apple","banana","cherry",sep="/")
+# apple|banana|cheery
+print("apple","banana","cherry" ,sep='-',end='$')
