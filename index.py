50122d4b010c612ffa1138df151f5666f4885ed8
Anandhbasha <anadhakm@gmail.com>
2025-06-25 13:33:51 +0530

    datatypes

--- index.py
+++ index.py
@@ -5,4 +5,33 @@ print("Hello Python is working", end=" ")
 print(a)
 print("apple","banana","cherry",sep="/")
 # apple|banana|cheery
-print("apple","banana","cherry" ,sep='-',end='$')
+print("apple","banana","cherry" ,sep='-',end='$\n')
+print(10,20,30,40 ,sep=' ', end='\tDone!\n')
+print("1","2","3","4",'5', sep='*', end='#\n')
+
+# 10 20 30 40 Done!
+#1*2*3*4*5#
+# variable declartion
+# a - a is the variable name
+# = assignent Opeartor
+# 10,20 are values of a
+
+# value is not more one is called as premitive
+# number int
+# 
+a=10
+# a[0] = 10
+a=20
+# a[0] = 20
+b=60
+a=b
+# a=60
+b=a
+print(a)
+print(b)
+# string
+name = "xyz"
+l='x'
+# boolean
+# true false
+isAlive = True
