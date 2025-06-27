56462fbc320acdd530d040a45a8852344cef62a0
Anandhbasha <anadhakm@gmail.com>
2025-06-27 13:26:29 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,36 @@
+# Arithmetic operator
+    # + addition,-Subtraction,*-Multiple,/- division,// - Floor Division,% - modulus,** Exponent
+
+a=50
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(2**2)
+
+# Assignment Operator
+    # = ,+=,-=,*=,/=,//=,%=,**=
+a+=b
+print(a)
+# a= a+b
+
+# comparision Opeartor
+# ==,<,<=,>,>=,!=
+# print(a==70 and a==50)
+# print(a<50)
+# print(a>50)
+# print(a>=50)
+# print(a<=50)
+# print(a!=b)
+
+# logical Opeartor
+# and or not
+age = 18
+location = "india"
+State = "UP"
+print(age>18 and location=="india" and State=="TN")
+print(age>18 and location=="india" or State=="TN")
+print(not age>18 )
