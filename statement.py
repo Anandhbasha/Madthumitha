04f73eaa57d94a3e789d9f83538694f207315125
Anandhbasha <anadhakm@gmail.com>
2025-07-01 20:54:35 +0530

    today

--- statement.py
+++ statement.py
@@ -51,3 +51,64 @@ if 'kiwi' in fruits:
 x=None
 if x is None:
     print('X is Empty')
+
+cMarks = 65
+mark12 = 60
+mark10=55
+if cMarks>=60:
+    if mark12>=60:
+        if mark10>=60:
+           print("Eligilble")
+        else:
+            print("Not Elible because of 10th")
+    else:
+        print("Not Elible based on 12th")
+else:
+    print("Not Elible based on Colledge")
+
+# loops
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+#     # i+=1
+
+newList = [50,60,80,90,100]
+for i in range(1,6):
+    print(i)
+
+for x in newList:
+    print(x)
+    # 1-x=50
+    # 2-x=60
+    # 3-x=80
+    # 4-x=90
+    # 5-x=100
+# print(newList[0])
+# print(newList[1])
+# print(newList[3])
+
+square = [x*x for x in range(1,6)]
+print(square)
+
+odd = [a for a in range(1,11) if a%2==0]
+print(odd)
+
+# swapping
+a,b=10,20
+# a=10
+# b=20
+a,b=b,a
+# a=b
+# b=a
+print(a,b)
+name = "madhumitha"
+# m
+# ma
+# mad
+# madh
+# madhu
+# madhumi
+# madhumit
+# madhumith
+# madhumitha
